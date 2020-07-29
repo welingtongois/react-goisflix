f26e7bf03ae3e7427192746597b481f3378fc5ed
noreplyicr <noreply.icr@hc.fm.usp.br>
2020-07-29 10:51:11 -0300

    Segundo commit :)

--- src/App.js
+++ src/App.js
@@ -5,11 +5,6 @@ function App() {
   return (
     <div>
       <Menu />
-      <h1>Hello World</h1>
-
-      Alguma coisa aqui<br />
-
-      Hot Reload
     </div >
   );
 }
--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import Logo from '../../assets/img/logo-flix.png';
+import '../Menu/Menu.css';
+import ButtonLink from './components/ButtonLink';
 
 function Menu() {
     return (
-        <header>
-            Goisflix
-        </header>
+        <nav className="Menu">
+            <a href="/">
+                <img className="Logo" src={Logo} alt="GoisFlix Logo" />
+            </a>
+
+            <ButtonLink className="ButtonLink" href="/">
+                Novo vídeo
+            </ButtonLink>
+        </nav>
     );
 }
-
 export default Menu;
