c9bdaac8960d6c11f3157ea9d1715faf4606228a
noreplyicr <noreply.icr@hc.fm.usp.br>
2020-07-29 12:51:29 -0300

    build

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React from 'react';
-import Menu from './components/Menu'
+import Menu from './components/Menu';
+import dadosIniciais from './data/dados_iniciais.json';
+import BannerMain from './components/BannerMain';
+import Carousel from './components/Carousel';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
       <Menu />
+
+      <BannerMain
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription={"Não adentre a boa noite apenas com ternura, não entre nessa noite acolhedora com ternura."}
+      />
+
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[0]}
+      />
+
+      <Carousel
+        category={dadosIniciais.categorias[1]}
+      />
+
+      <Footer />
     </div >
   );
 }
